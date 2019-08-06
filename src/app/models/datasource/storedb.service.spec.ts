977485e8acd2d6a31aa6ead7d7d10bd1b89266a5
george.orhewere@gmail.com <george.orhewere@gmail.com>
2019-08-06 20:22:07 +0100

    all basic initial tests run successfully

--- src/app/models/datasource/storedb.service.spec.ts
+++ src/app/models/datasource/storedb.service.spec.ts
@@ -1,10 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 import { StoreDBService } from './storedb.service';
 
 describe('StoredbService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  /* beforeEach(() => TestBed.configureTestingModule({
+    imports:[ HttpClient ]
+  })); */
+  let service:HttpClient;
+  
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({    
+   providers:[ {provide: HttpClient, useValue: service }]    
+    })
+    .compileComponents();
+  }));
   it('should be created', () => {
     const service: StoreDBService = TestBed.get(StoreDBService);
     expect(service).toBeTruthy();
